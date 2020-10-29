58bf02079d0d8fd58911d3b326fd90917047b4a7
denimcouch <alexmata92@ymail.com>
2020-10-29 15:33:28 -0500

    Done.

--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -2,14 +2,36 @@ import React, { Component } from 'react';
 
 class ToyForm extends Component {
 
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      image: '',
+      likes: 0
+    }
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.addNewToy(this.state)
+  }
+
+  handleChange = (event) => {
+    let {name, value} = event.target
+    console.log(name, value)
+    this.setState({
+      [name]: value
+    })
+  }
+
   render() {
     return (
       <div className="container">
-        <form className="add-toy-form">
+        <form className="add-toy-form" onSubmit={this.handleSubmit}>
           <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
+          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text" onChange={this.handleChange}/>
           <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
+          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text" onChange={this.handleChange}/>
           <br/>
           <input type="submit" name="submit" value="Create New Toy" className="submit"/>
         </form>
